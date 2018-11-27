82a2106745d61fca16978a40e98a6bc3d578fffb
Jairam Gottapu <Jairam.Gottapu@itiviti.com>
2018-11-27 00:40:20 +0000

    Added missing documentation.

--- src/main/java/com/mycompany/shoppingcart/Main.java
+++ src/main/java/com/mycompany/shoppingcart/Main.java
@@ -11,6 +11,9 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Example main class to illustrate how to run the program.
+ */
 public class Main {
 
   public static void main(String[] args) {
--- src/main/java/com/mycompany/shoppingcart/discount/Discount.java
+++ src/main/java/com/mycompany/shoppingcart/discount/Discount.java
@@ -2,6 +2,9 @@ package com.mycompany.shoppingcart.discount;
 
 import com.mycompany.shoppingcart.cart.CartEntry;
 
+/**
+ * Interface to be implemented to provide discount for cart entries.
+ */
 public interface Discount {
 
   void apply(CartEntry entry);
